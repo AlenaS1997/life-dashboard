1fcf7dbd1d731130d6f7cb11f4a10a049242aebc
Alena Savchenko <savchenko.alena.27071997@gmail.com>
2026-05-07 13:09:09 +0300

    Дашборд: heatmap сна как календарная сетка 7 колонок (пн..вс)

--- dashboard/sw.js
+++ dashboard/sw.js
@@ -11,7 +11,7 @@
 // При выходе новой версии меняй CACHE_NAME — старые кэши будут удалены при
 // активации нового SW.
 
-const CACHE_NAME = "life-dashboard-v11";
+const CACHE_NAME = "life-dashboard-v12";
 const CORE_ASSETS = [
   "./",
   "./index.html",
