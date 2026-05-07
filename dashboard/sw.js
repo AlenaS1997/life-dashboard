07cfcf189656344a33950ef034973fa5191cd1d8
Alena Savchenko <savchenko.alena.27071997@gmail.com>
2026-05-07 12:56:39 +0300

    Дашборд: тренды с tap-показом значений, heatmap 14/30 с тоглом, фазы сна с подписями часов, отбой/подъём только полосы; ранее — фикс UTC и Atwater-ккал

--- dashboard/sw.js
+++ dashboard/sw.js
@@ -11,7 +11,7 @@
 // При выходе новой версии меняй CACHE_NAME — старые кэши будут удалены при
 // активации нового SW.
 
-const CACHE_NAME = "life-dashboard-v10";
+const CACHE_NAME = "life-dashboard-v11";
 const CORE_ASSETS = [
   "./",
   "./index.html",
